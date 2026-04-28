5251134dd73594ba6a961dc4ed5082cda23e93f9
sakshi-builds <sharmasakshi1117@gmail.com>
2026-04-28 18:48:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-	 get name() {
+    constructor(name, age) {
+        this._name = name;   // ✅ fixed
+        this._age = age;
+    }
+
+    get name() {
         return this._name;
     }
 
@@ -18,17 +19,16 @@ class Person {
 }
 
 class Student extends Person {
-	study(){
-		console.log(this.name +" is studying");
-	}
+    study() {
+        console.log(this.name + " is studying");
+    }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(this.name + " is teaching");
-	}
+    teach() {
+        console.log(this.name + " is teaching");
+    }
 }
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
